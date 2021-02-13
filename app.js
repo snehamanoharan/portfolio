bb4b071b2287004f805415def681e9b3f96670d4
SnehaManoharan <snehamanoharan992@gmail.com>
2021-02-13 16:14:43 +0530

    portfolio
    code

---
+++ app.js
@@ -0,0 +1,37 @@
+/* var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    Mesaage = require("./model");
+
+
+
+
+/*seeddb
+mongoose.connect('mongodb://localhost:27017/portfolio', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+/*function*/
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+function toggleMenu() {
+    var menuToggle = document.querySelector(".toggle");
+    menuToggle.classList.toggle("active");
+    var menu = document.querySelector(".menu");
+    menu.classList.toggle("active");
+
+}
+
+/*port
+app.listen(3000, function() {
+    console.log("Yelp camp server has started in port 3000");
+});*/
